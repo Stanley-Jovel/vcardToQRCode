5a748b6d0d196d742b6d7a49f889abfc26f12641
Luis <luigi.lsjp@gmail.com>
2016-06-14 18:43:32 -0700

    small fiex

--- config/env/production.js
+++ config/env/production.js
@@ -28,7 +28,7 @@ module.exports = {
   },
   hostname: 'http://localhost:3000',
   app: {
-    name: 'MEAN - A Modern Stack - Production'
+    name: 'vCard to QR Code'
   },
   logging: {
     format: 'combined'
